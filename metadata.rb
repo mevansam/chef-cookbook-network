9987da023697a5fac1c18fec422df3f9007eb172
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-08-08 12:21:34 -0400

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'network'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures network'
+long_description 'Installs/Configures network'
+version          '0.1.0'
+
